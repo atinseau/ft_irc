e80378933c0a4d136a07484d936abd7de706bdfe
Marc-Etienne Bonnet <webbonnet@gmail.com>
2022-05-17 10:11:22 +0200

    netoyage

--- src/socket/socket.cpp
+++ src/socket/socket.cpp
@@ -2,9 +2,7 @@
 
 void	msgServer(std::string str)
 {
-	std::cerr << VIOLET << "------------------------------------------------------" << std::endl;
-	std::cerr << "SERVER : " << str << std::endl;
-	std::cerr << "------------------------------------------------------" << BLANC << std::endl;
+	std::cerr << VIOLET << "---SERVER---  " << str << BLANC << std::endl;
 }
 
 Server::Server(void)
@@ -12,52 +10,24 @@ Server::Server(void)
 	int rc;
 	int on = 1;
 
-  this->_sockServer = socket(AF_INET6, SOCK_STREAM, 0);
-  if (this->_sockServer < 0)
-  {
-    perror("socket() failed");
-    exit(-1);
-  }
-
-  rc = setsockopt(this->_sockServer, SOL_SOCKET,  SO_REUSEADDR,
-                  (char *)&on, sizeof(on));
-  if (rc < 0)
-  {
-    perror("setsockopt() failed");
-    close(this->_sockServer);
-    exit(-1);
-  }
-  rc = ioctl(this->_sockServer, FIONBIO, (char *)&on);
-  if (rc < 0)
-  {
-    perror("ioctl() failed");
-    close(this->_sockServer);
-    exit(-1);
-  }
-  memset(&this->_addrServer, 0, sizeof(this->_addrServer));
-  this->_addrServer.sin6_family      = AF_INET6;
-  memcpy(&this->_addrServer.sin6_addr, &in6addr_any, sizeof(in6addr_any));
-  this->_addrServer.sin6_port        = htons(SERVER_PORT);
-  rc = bind(this->_sockServer,
-            (struct sockaddr *)&this->_addrServer, sizeof(this->_addrServer));
-  if (rc < 0)
-  {
-    perror("bind() failed");
-    close(this->_sockServer);
-    exit(-1);
-  }
-  rc = listen(this->_sockServer, 32);
-  if (rc < 0)
-  {
-    perror("listen() failed");
-    close(this->_sockServer);
-    exit(-1);
-  }
-  memset(this->_pfds, 0 , sizeof(this->_pfds));
-
-  this->_pfds[0].fd = this->_sockServer;
-  this->_pfds[0].events = POLLIN;
-  
+	if ((this->_sockServer = socket(AF_INET6, SOCK_STREAM, 0)) < 0)
+		throw std::runtime_error(ROUGE"socket() failed"BLANC);
+	if ((rc = setsockopt(this->_sockServer, SOL_SOCKET,  SO_REUSEADDR,(char *)&on, sizeof(on))) < 0)
+		throw std::runtime_error(ROUGE"setsockopt() failed"BLANC);
+	if ((rc = ioctl(this->_sockServer, FIONBIO, (char *)&on)) < 0)
+		throw std::runtime_error(ROUGE"ioctl() failed"BLANC);
+	memset(&this->_addrServer, 0, sizeof(this->_addrServer));
+	this->_addrServer.sin6_family      = AF_INET6;
+	memcpy(&this->_addrServer.sin6_addr, &in6addr_any, sizeof(in6addr_any));
+	this->_addrServer.sin6_port        = htons(SERVER_PORT);
+	if ((rc = bind(this->_sockServer,(struct sockaddr *)&this->_addrServer, sizeof(this->_addrServer))) < 0)
+			throw std::runtime_error(ROUGE"bind() failed"BLANC);
+	if ((rc = listen(this->_sockServer, 32)) < 0)
+				throw std::runtime_error(ROUGE"listen() failed"BLANC);
+	memset(this->_pfds, 0 , sizeof(this->_pfds));
+	this->_pfds[0].fd = this->_sockServer;
+	this->_pfds[0].events = POLLIN;
+	msgServer("Server correctement initialiser...");
 }
 
 Server::~Server()
@@ -68,207 +38,98 @@ Server::~Server()
 	} 
 }
 
-//void Server::addClient(int &lenTab)
-//{
-	
-//}
-
-/*
 void Server::run(void)
 {
-	int resPoll;
-	int sizeVector;
-	int lenTab = 1;
-	int fd;
-
-	while (1)
+	int rc, len, new_sd, current_size;
+	int nfds = 1;
+	bool end_server, close_conn, compress_array;
+	char buffer[200];
+	do
 	{
-		msgServer("Attente de poll()...");
-		resPoll = poll(this->_pfds, lenTab, TIME);
-		if (resPoll < 0)
+		msgServer("En attente de poll()...");
+		rc = poll(this->_pfds, nfds, TIME);
+		if (rc < 0)
+			throw std::runtime_error(ROUGE"poll() failed"BLANC);
+		if (rc == 0)
+			throw std::runtime_error(ROUGE"poll() timeout"BLANC);
+		current_size = nfds;
+		for (int i = 0; i < current_size; i++)
 		{
-			throw std::runtime_error(ROUGE"Poll() failed"BLANC);
-		}
-		if (resPoll == 0)
-		{
-			msgServer("La fonction poll() est arrivée a la fin de sont timer...");
-			break;
-		}
-		sizeVector = lenTab;
-		for (int i = 0; i < sizeVector; i++)
-		{
-			if (this->_pfds[i].revents == 0)
-				continue ;
-			if (this->_pfds[i].revents != POLLIN)
+			if(this->_pfds[i].revents == 0)
+				continue;
+			if(this->_pfds[i].revents != POLLIN)
 			{
-				throw std::runtime_error(ROUGE"ERREUR d unn client"BLANC);
+				std::cout << ROUGE << "Error! revents = " << this->_pfds[i].revents << BLANC << std::endl;
+				end_server = true;
+				break;
 			}
 			if (this->_pfds[i].fd == this->_sockServer)
 			{
-				msgServer("Recherche de client en cours...");
-				do 
+				msgServer("Listening socket is readable");
+				do
 				{
-					if ((fd = accept(this->_sockServer, NULL, NULL)) < 0)
+					new_sd = accept(this->_sockServer, NULL, NULL);
+					if (new_sd < 0)
 					{
-						close(fd);
-						throw std::runtime_error(ROUGE"Accept() failed"BLANC);
-					} 
-					msgServer("connecter...");
-					this->_pfds[lenTab].fd = fd;
-					this->_pfds[lenTab].events = POLLIN;
-					lenTab++;
-				} while (fd != -1);
+						if (errno != EWOULDBLOCK)
+							throw std::runtime_error(ROUGE"accept() failed"BLANC);
+						break;
+					}
+					std::cerr << VIOLET << "---SERVER---  " << "Nouvel connection ! fd : " << new_sd << BLANC << std::endl;
+					this->_pfds[nfds].fd = new_sd;
+					this->_pfds[nfds].events = POLLIN;
+					nfds++;
+				} while (new_sd != -1);
 			}
 			else
 			{
-				std::cerr << VIOLET << "------------------------------------------------------" << std::endl;
-				std::cerr << "SERVER : " << "Fd : " << this->_pfds[i].fd << " est lisible..." << std::endl;
-				std::cerr << "------------------------------------------------------" << BLANC << std::endl;
-				int resRecv = 0;
-				std::string tmp;
-				while (1)
+				std::cerr << VIOLET << "---SERVER---  Descripteur" << this->_pfds[i].fd << " est lisible..." << BLANC << std::endl;
+				close_conn = false;
+				do
 				{
-					std::cout << "test ici" << std::endl;
-					resRecv = recv(this->_pfds[i].fd, &tmp, sizeof(tmp), 0);
-					if (resRecv < 0)
-						throw std::runtime_error(ROUGE"Recv() failed"BLANC);
-					if (resRecv == 0)
+					rc = recv(this->_pfds[i].fd, buffer, sizeof(buffer), 0);
+					if (rc < 0)
 					{
-						msgServer("Connection client fermée");
-						break ;
+						if (errno != EWOULDBLOCK)
+							throw std::runtime_error(ROUGE"recv() failed"BLANC);
+						break;
 					}
-					std::cerr << VIOLET << "------------------------------------------------------" << std::endl;
-					std::cerr << "SERVER : " << resRecv << " on etait ressus : \"" << tmp << " est lisible..." << std::endl;
-					std::cerr << "------------------------------------------------------" << BLANC << std::endl;
-					resRecv = send(this->_pfds[i].fd, &tmp, sizeof(tmp), 0);
-					if (resRecv < 0)
-						throw std::runtime_error(ROUGE"Send() failed"BLANC);
+					if (rc == 0)
+					{
+						msgServer("Connection closed\n");
+						close_conn = true;
+						break;
+					}
+					len = rc;
+					std::cerr << BLEU << "---SERVER---  Reception : " << len << " bits" << BLANC << std::endl;
+					std::cerr << BLEU << "---SERVER---  Reception : \" " << buffer << " \"" << BLANC << std::endl;
+					rc = send(this->_pfds[i].fd, buffer, len, 0);
+					if (rc < 0)
+						throw std::runtime_error(ROUGE"send() failed"BLANC);
+				} while(true);
+				if (close_conn)
+				{
+					close(this->_pfds[i].fd);
+					this->_pfds[i].fd = -1;
+					compress_array = true;
 				}
-				
 			}
-
+		} 
+		if (compress_array)
+		{
+			compress_array = false;
+			for (int i = 0; i < nfds; i++)
+			{
+				if (this->_pfds[i].fd == -1)
+				{
+					for(int j = i; j < nfds-1; j++)
+					{
+						this->_pfds[j].fd = this->_pfds[j+1].fd;
+					}
+					i--;
+					nfds--;
+				}
+			}
 		}
-	}
-		
-}*/
-
-void Server::run(void)
-{
-	int rc;
-	int len;
-	int new_sd;
-	int nfds = 1;
-	int current_size;
-	int i;
-	bool end_server;
-	bool close_conn;
-	bool compress_array;
-	char buffer[200];
-	do
-  {
-    printf("Waiting on poll()...\n");
-    rc = poll(this->_pfds, nfds, TIME);
-    if (rc < 0)
-    {
-      perror("  poll() failed");
-      break;
-    }
-    if (rc == 0)
-    {
-      printf("  poll() timed out.  End program.\n");
-      break;
-    }
-    current_size = nfds;
-    for (i = 0; i < current_size; i++)
-    {
-      if(this->_pfds[i].revents == 0)
-        continue;
-      if(this->_pfds[i].revents != POLLIN)
-      {
-        printf("  Error! revents = %d\n", this->_pfds[i].revents);
-        end_server = true;
-        break;
-      }
-      if (this->_pfds[i].fd == this->_sockServer)
-      {
-        printf("  Listening socket is readable\n");
-        do
-        {
-         
-          new_sd = accept(this->_sockServer, NULL, NULL);
-          if (new_sd < 0)
-          {
-            if (errno != EWOULDBLOCK)
-            {
-              perror("  accept() failed");
-              end_server = true;
-            }
-            break;
-          }
-          printf("  New incoming connection - %d\n", new_sd);
-          this->_pfds[nfds].fd = new_sd;
-          this->_pfds[nfds].events = POLLIN;
-          nfds++;
-        } while (new_sd != -1);
-      }
-      else
-      {
-        printf("  Descriptor %d is readable\n", this->_pfds[i].fd);
-        close_conn = false;
-        do
-        {
-          rc = recv(this->_pfds[i].fd, buffer, sizeof(buffer), 0);
-          if (rc < 0)
-          {
-            if (errno != EWOULDBLOCK)
-            {
-              perror("  recv() failed");
-              close_conn = true;
-            }
-            break;
-          }
-          if (rc == 0)
-          {
-            printf("  Connection closed\n");
-            close_conn = true;
-            break;
-          }
-          len = rc;
-          printf("  %d bytes received\n", len);
-          rc = send(this->_pfds[i].fd, buffer, len, 0);
-          if (rc < 0)
-          {
-            perror("  send() failed");
-            close_conn = true;
-            break;
-          }
-
-        } while(true);
-        if (close_conn)
-        {
-          close(this->_pfds[i].fd);
-          this->_pfds[i].fd = -1;
-          compress_array = true;
-        }
-      }  
-    } 
-    if (compress_array)
-    {
-      compress_array = false;
-      for (i = 0; i < nfds; i++)
-      {
-        if (this->_pfds[i].fd == -1)
-        {
-          for(int j = i; j < nfds-1; j++)
-          {
-        	this->_pfds[j].fd = this->_pfds[j+1].fd;
-          }
-          i--;
-          nfds--;
-        }
-      }
-    }
-
-  } while (end_server == false); /* End of serving running.    */
-
+	} while (end_server == false); /* End of serving running.    */
 }
